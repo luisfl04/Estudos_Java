d9ec7fe9b5e4938d12c463a34ef6c60d5aa35cc1
luis felipe <luisfelipeconato08@gmail.com>
2024-05-02 20:39:33 -0300

    feat: remoção do arquivo 'TesteImportação.java'

--- src/est/luis/main/Program.java
+++ src/est/luis/main/Program.java
@@ -5,25 +5,8 @@ public class Program{
 
     public static void main(String[] args){
         
-        Som teste = new Program().new Aguia();
-        teste.reproduzirSom("UAAAAAAAA");
     }    
-    
-    public abstract class Som {
-    
-        public abstract void reproduzirSom(String texto);
-    
-    }
-    
-    public class Aguia extends Som{
 
-        @Override
-        public void reproduzirSom(String texto){
-            System.out.println(texto);
-        }
-
-    } 
-    
 }
 
 
